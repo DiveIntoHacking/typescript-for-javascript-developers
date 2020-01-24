f769d4c51ee451e240250cab2ff120cc916c63b3
Atsushi Ishida <gipcompany@gmail.com>
2020-01-25 03:13:50 +0900

    add never.ts

---
+++ src/never.ts
@@ -0,0 +1,15 @@
+export {};
+
+function error(message: string): never {
+  throw new Error(message);
+}
+
+try {
+  let result = error('test');
+  console.log({ result });
+} catch (error) {
+  console.log({ error });
+}
+
+let foo: void = undefined;
+let bar: never = error('only me!');
