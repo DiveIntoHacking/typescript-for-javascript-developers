4b7f3955df6dbac88437568f47aea343fc52a25e
Atsushi Ishida <gipcompany@gmail.com>
2020-01-24 16:14:52 +0900

    add array.ts

---
+++ src/array.ts
@@ -0,0 +1,15 @@
+export {};
+
+let numbers: number[] = [1, 2, 3];
+
+let numbers2: Array<number> = [1, 2, 3];
+let strings2: Array<string> = ['Tokyo', 'Osaka', 'Kyoto'];
+
+let strings: string[] = ['TypeScript', 'JavaScript', 'CoffeeScript'];
+
+let nijigenHairetsu: number[][] = [
+  [50, 100],
+  [150, 300]
+];
+
+let hairetsu: (string | number | boolean)[] = [1, false, 'Japan'];
