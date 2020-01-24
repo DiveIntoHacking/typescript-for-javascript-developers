d4fdeb616935042c0fcb29c03fefb2ef0304b91b
Atsushi Ishida <gipcompany@gmail.com>
2020-01-24 14:56:14 +0900

    add number-string.ts

---
+++ src/number-string.ts
@@ -0,0 +1,13 @@
+export {};
+
+let year: number = 1976;
+// year = false;
+console.log(year);
+
+let age: number = 0x2b;
+
+console.log(age);
+
+let name: string = 'Ham';
+// name = 10;
+console.log(name);
