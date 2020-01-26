de265ac11a0d2837829e22f92fc945d38abeeffe
Atsushi Ishida <gipcompany@gmail.com>
2020-01-26 16:39:18 +0900

    add interfaces.ts

---
+++ src/interfaces.ts
@@ -0,0 +1,16 @@
+export {};
+
+type ObjectType = {
+  name: string;
+  age: number;
+};
+
+interface ObjectInterface {
+  name: string;
+  age: number;
+}
+
+let object: ObjectInterface = {
+  name: 'Ham-san',
+  age: 43
+};
