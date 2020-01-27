31bdd702a0725e7ee424b7f67f837057313b19e9
Atsushi Ishida <gipcompany@gmail.com>
2020-01-28 02:42:16 +0900

    add literal.ts

---
+++ src/literal.ts
@@ -0,0 +1,12 @@
+export {};
+
+let dayOfTheWeek: '日' | '月' | '火' | '水' | '木' | '金' | '土' = '日';
+dayOfTheWeek = '月';
+// dayOfTheWeek = '31';
+
+let month: 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9 | 10 | 11 | 12 = 1;
+month = 12;
+// month = 13;
+
+let TRUE: true = true;
+// TRUE = false;
