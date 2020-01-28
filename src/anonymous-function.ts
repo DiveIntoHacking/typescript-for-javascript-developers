0a13e0c53718d51de08e39ccefcdb57687e6ac1f
Atsushi Ishida <gipcompany@gmail.com>
2020-01-28 11:15:17 +0900

    add anonymous-function.ts

---
+++ src/anonymous-function.ts
@@ -0,0 +1,10 @@
+export {};
+
+let bmi: (height: number, weight: number) => number = function(
+  height: number,
+  weight: number
+): number {
+  return weight / (height * height);
+};
+
+console.log(bmi(1.78, 86));
