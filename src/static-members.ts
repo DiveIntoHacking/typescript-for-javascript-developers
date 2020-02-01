5a64cc9671d8c484ef5f5810c67ce4e04ec6a1ce
Atsushi Ishida <gipcompany@gmail.com>
2020-02-01 13:42:00 +0900

    add static-members.ts

---
+++ src/static-members.ts
@@ -0,0 +1,17 @@
+export {};
+
+class Me {
+  static isProgrammer: boolean = true;
+  static firstName: string = 'Atsushi';
+  static lastName: string = 'Ishida';
+
+  static work() {
+    //     "Hey, guys! This is Atsushi! Are you interested in TypeScript? Let's dive into TypeScript!";
+    return `Hey, guys! This is ${this.firstName}! Are you interested in TypeScript? Let's dive into TypeScript!`;
+  }
+}
+
+// let me = new Me();
+// console.log(me.isProgrammer);
+console.log(Me.isProgrammer);
+console.log(Me.work());
