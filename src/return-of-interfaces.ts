53a8a0000440b5c9817d63050801a693d674a65e
Atsushi Ishida <gipcompany@gmail.com>
2020-02-02 14:37:21 +0900

    add return-of-interfaces.ts

---
+++ src/return-of-interfaces.ts
@@ -0,0 +1,28 @@
+export {};
+
+class Mahotsukai {}
+class Souryo {}
+
+class Taro extends Mahotsukai {}
+
+interface Kenja {
+  ionazun(): void;
+}
+
+interface Senshi {
+  kougeki(): void;
+}
+
+class Jiro implements Kenja, Senshi {
+  ionazun(): void {
+    console.log('iozazun');
+  }
+
+  kougeki(): void {
+    console.log('kougeki');
+  }
+}
+
+const jiro = new Jiro();
+jiro.ionazun();
+jiro.kougeki();
