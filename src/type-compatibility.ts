bf8ae0f1fbfe1ad904b396d67a9d8c3082eb1bff
Atsushi Ishida <gipcompany@gmail.com>
2020-02-02 16:06:49 +0900

    add type-compatibility.ts

---
+++ src/type-compatibility.ts
@@ -0,0 +1,39 @@
+export {};
+
+let fooCompatible: any;
+let barCompatible: string = 'TypeScript';
+
+console.log(typeof fooCompatible);
+
+fooCompatible = barCompatible;
+
+console.log(typeof fooCompatible);
+
+let fooIncompatible: string;
+let barIncompatible: number = 1;
+
+// fooIncompatible = barIncompatible;
+
+let fooString: string;
+let barString: string = 'string';
+
+fooString = barString;
+
+let fooStringLiteral: 'fooStringLiteral' = 'fooStringLiteral';
+fooString = fooStringLiteral;
+
+let fooNumber: number;
+let fooNumberLiteral: 1976 = 1976;
+fooNumber = fooNumberLiteral;
+
+interface Animal {
+  age: number;
+  name: string;
+}
+
+class Person {
+  constructor(public age: number, public name: string) {}
+}
+
+let me: Animal;
+me = new Person(43, 'HamSan');
