ebdaef49567335dc1749902548db556734c98e9b
Atsushi Ishida <gipcompany@gmail.com>
2020-02-10 17:47:07 +0900

    example for axios with proxy

--- src/any.ts
+++ src/any.ts
@@ -5,7 +5,20 @@ export {};
 let url: string =
   'https://udemy-utils.herokuapp.com/api/v1/articles?token=token123';
 
-axios.get(url).then(function(response) {
+// https://github.com/axios/axios#request-config
+// にconfigに設定可能なパラメータの説明があります。
+// 説明に記載の通り、urlのみ必須のパラメータのようです。
+// 以下はサンプルです。適宜、使用されているproxyのhost、portに書き換えて使用してください。
+// 認証情報authも必要であればREADMEを参照しながら追加してください。
+const config = {
+  url,
+  proxy: {
+    host: '127.0.0.1',
+    port: 8080
+  }
+};
+
+axios.get(url, config).then(function(response) {
   interface Article {
     id: number;
     title: string;
