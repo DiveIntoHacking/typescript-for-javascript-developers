5f7fa37ec9bf6729f34b0aa36362dbad7d8a27f6
Atsushi Ishida <gipcompany@gmail.com>
2020-07-25 23:57:04 +0900

    Exclude, Extract, NonNullable

---
+++ src/exclude-extract-nonnullable.ts
@@ -0,0 +1,14 @@
+export {};
+
+type DebugType = () => void;
+type SomeTypes = string | number | DebugType;
+type FunctionType = Exclude<SomeTypes, string | number>;
+type NonFunctionType = Exclude<SomeTypes, DebugType>;
+type TypeExcludingFunction = Exclude<SomeTypes, Function>;
+
+type FunctionTypeByExtract = Extract<SomeTypes, DebugType>;
+type NonFunctionTypeByExtract = Extract<SomeTypes, string | number>;
+type FunctionTypeExtractingFunction = Extract<SomeTypes, Function>;
+
+type NullableTypes = string | number | null | undefined;
+type NonNullableTypes = NonNullable<NullableTypes>;
