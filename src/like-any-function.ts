d6c609a9c4bcedd20b90d4ebb09a523fcb508dd6
Atsushi Ishida <gipcompany@gmail.com>
2021-02-18 23:31:34 +0900

    one solution

--- src/like-any-function.ts
+++ src/like-any-function.ts
@@ -1,14 +1,16 @@
 export {};
 
-type Any<T extends readonly any[]> = T[number] extends 0
+type Any<T extends any[]> = T[number] extends 0
   ? false
   : T[number] extends ''
   ? false
   : T[number] extends false
+  ? false
+  : T[number] extends 0 | '' | false
   ? false
   : true;
 
 type Test1 = Any<[0]>; // false
 type Test2 = Any<['']>; // false
 type Test3 = Any<[false]>; // false
-type Test4 = Any<[0, '', false]>; // true
+type Test4 = Any<[0, '', false]>; // false
